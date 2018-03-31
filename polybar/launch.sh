8f5e9edf9baec1a1cc920a7de941c78be8ae3b9b
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-04-01 00:19:55 +0430

    fix: clean up some stuff

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,13 +10,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload -c ~/.config/polybar/config main &
-    # MONITOR=$m polybar --reload -c ~/.config/polybar/config time &
-    # MONITOR=$m polybar --reload -c ~/.config/polybar/config i3 &
   done
 else
   polybar --reload -c ~/.config/polybar/config main &
-  # polybar --reload -c ~/.config/polybar/config time &
-  # polybar --reload -c ~/.config/polybar/config i3 &
 fi
 
 
