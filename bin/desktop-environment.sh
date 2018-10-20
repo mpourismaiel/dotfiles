a433ae9f6f194b8e1721f360500795565dac2ce1
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-10-20 16:32:30 +0330

    Update keyboard configuration

---
+++ bin/desktop-environment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 0.5
+xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+sleep 0.5
+i3-msg "restart"
