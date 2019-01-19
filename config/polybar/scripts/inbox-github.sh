346c7f5524275a19ca0ada5914ca611943dd0247
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-01-20 03:14:02 +0330

    Update awesome theme

--- config/polybar/scripts/inbox-github.sh
+++ config/polybar/scripts/inbox-github.sh
@@ -6,5 +6,5 @@ TOKEN=$(cat $HOME/.tokens/github)
 notifications=$(curl -fs https://api.github.com/notifications?access_token=$TOKEN | jq ".[].unread" | grep -c true)
 
 if [ ! -z $notifications ] && [ "$notifications" -gt 0 ]; then
-  echo " $notifications"
+  echo "$notifications"
 fi
