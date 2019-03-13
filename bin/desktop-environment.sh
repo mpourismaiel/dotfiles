c3cc6e9a00266e58bdc08f787defc71c142db524
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-03-14 00:47:17 +0330

    Update

--- bin/desktop-environment.sh
+++ bin/desktop-environment.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
-sleep 0.5
-MONITOR_COUNT=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+#!/usr/bin/zsh
 
-if [ $MONITOR_COUNT -eq 1 ]
-then
-  sh $HOME/.screenlayout/single-screen-full-hd.sh
+export DISPLAY=:0
+export XAUTHORITY=/home/mahdi/.Xauthority
+
+sleep 0.5
+IS_HDMI_CONNECTED=$(xrandr --query | grep -E "HDMI.*\sconnected" | wc -l)
+echo "HOW MANY CONNECTED?"
+echo $IS_HDMI_CONNECTED
+if [ $IS_HDMI_CONNECTED -eq 1 ]; then
+  echo "Initiating second screen"
+  /usr/bin/sh /home/mahdi/.screenlayout/single-screen-second-full-hd.sh
 else
-  sh $HOME/.screenlayout/single-screen-second-full-hd.sh
+  echo "Initiating builtin screen"
+  /usr/bin/sh /home/mahdi/.screenlayout/single-screen-full-hd.sh
 fi
 
 sleep 0.5
-i3-msg "restart"
+echo "awesome.restart()" | /usr/bin/awesome-client
--- bin/show-ping.sh
+++ bin/show-ping.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+PID=$(cat /tmp/polybar-ping)
+if [ $PID ]; then
+  if [ ! -d /proc/$PID ]; then
+    ping-log&
+  fi
+else
+  ping-log&
+fi
+
 tail -n 1 /tmp/ping-log | sed s/.*time=//g
