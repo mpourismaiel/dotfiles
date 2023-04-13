995dda50fad1f9597bfc725c121784dd9f9b7044
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2023-04-13 23:21:21 +0330

    Group same class clients in tasklist

--- configuration/widgets/tasklist/init.lua
+++ configuration/widgets/tasklist/init.lua
@@ -7,39 +7,30 @@ local config = require("configuration.config")
 local tasklist = {mt = {}}
 
 local function create_buttons(buttons, object)
-  local is_formatted = buttons and buttons[1] and (type(buttons[1]) == "button" or buttons[1]._is_capi_button) or false
-
-  if buttons then
-    local btns = {}
-    for _, src in ipairs(buttons) do
-      --TODO v6 Remove this legacy overhead
-      for _, b in ipairs(is_formatted and {src} or src) do
-        -- Create a proxy button object: it will receive the real
-        -- press and release events, and will propagate them to the
-        -- button object the user provided, but with the object as
-        -- argument.
-        local btn = capi.button {modifiers = b.modifiers, button = b.button}
-        btn:connect_signal(
-          "press",
-          function()
-            b:emit_signal("press", object)
-          end
-        )
-        btn:connect_signal(
-          "release",
-          function()
-            b:emit_signal("release", object)
-          end
-        )
-        btns[#btns + 1] = btn
-      end
+  local btns = {}
+  for _, src in ipairs(buttons) do
+    for _, b in ipairs(src) do
+      local btn = capi.button {modifiers = b.modifiers, button = b.button}
+      btn:connect_signal(
+        "press",
+        function()
+          b:emit_signal("press", object)
+        end
+      )
+      btn:connect_signal(
+        "release",
+        function()
+          b:emit_signal("release", object)
+        end
+      )
+      btns[#btns + 1] = btn
     end
-
-    return btns
   end
+
+  return btns
 end
 
-local function custom_template(args)
+local function custom_template(client_count)
   local template = {
     id = "background",
     border_strategy = "inner",
@@ -67,103 +58,204 @@ local function custom_template(args)
             widget = wibox.container.place,
             halign = "left",
             {
-              id = "indicator",
-              widget = wibox.container.background,
-              shape = gears.shape.rounded_rect,
-              {
-                widget = wibox.container.constraint,
-                strategy = "exact",
-                width = config.dpi(3),
-                height = config.dpi(12),
-                {
-                  widget = wibox.widget.textbox
-                }
-              }
+              id = "indicator_list",
+              layout = wibox.layout.fixed.vertical,
+              spacing = config.dpi(3)
             }
           }
         }
       }
     }
   }
+
   local l = wibox.widget.base.make_widget_from_value(template)
+  local indicator_list = l:get_children_by_id("indicator_list")[1]
+
+  for i = 1, client_count do
+    indicator_list:add(
+      wibox.widget.base.make_widget_from_value {
+        widget = wibox.container.background,
+        shape = gears.shape.rounded_rect,
+        {
+          widget = wibox.container.constraint,
+          id = "size",
+          strategy = "exact",
+          width = config.dpi(3),
+          height = config.dpi(6),
+          {
+            widget = wibox.widget.textbox
+          }
+        }
+      }
+    )
+  end
 
   return {
     background = l:get_children_by_id("background")[1],
     icon = l:get_children_by_id("icon")[1],
-    indicator = l:get_children_by_id("indicator")[1],
+    indicator = indicator_list.children,
     primary = l,
     update_callback = l.update_callback,
     create_callback = l.create_callback
   }
 end
 
-function tasklist.render(w, buttons, label, data, objects, args)
-  -- update the widgets, creating them if needed
+function tasklist.render(w, buttons, label, widgets_cache, objects, args)
   w:reset()
   for i, o in ipairs(objects) do
-    local cache = data[o]
-
-    -- Allow the buttons to be replaced.
-    if cache and cache._buttons ~= buttons then
-      cache = nil
-    end
+    local cache = widgets_cache[o]
 
     if not cache then
-      cache = custom_template()
+      cache = {
+        w = custom_template(#o.clients),
+        children = {},
+        length = #o.clients,
+        class = o.class,
+        clients = o.clients,
+        popup = nil
+      }
 
-      cache.primary.buttons = {create_buttons(buttons, o)}
+      if cache.length == 1 then
+        cache.w.primary.buttons = {create_buttons(buttons.client, o.clients[1])}
+      else
+        cache.w.primary.buttons = {create_buttons(buttons.group, cache)}
+      end
 
-      if cache.create_callback then
-        cache.create_callback(cache.primary, o, i, objects)
+      if cache.w.create_callback then
+        cache.w.create_callback(cache.w.primary, o, i, objects)
       end
 
       cache._buttons = buttons
-      data[o] = cache
-    elseif cache.update_callback then
-      cache.update_callback(cache.primary, o, i, objects)
+      widgets_cache[o] = cache
     end
 
-    if o.icon then
-      cache.icon:set_image(o.icon)
+    if o.clients[1].icon then
+      cache.w.icon:set_image(o.clients[1].icon)
     end
 
-    cache.indicator.bg = "#ffffff00"
-    if
-      o.active or
-        (capi.client.focus and capi.client.focus.skip_taskbar and
-          capi.client.focus:get_transient_for_matching(
-            function(cl)
-              return not cl.skip_taskbar
-            end
-          ) == o)
-     then
-      cache.indicator.bg = "#ffffff88"
+    -- loop through cache.w.indicator
+    for _, c in ipairs(cache.w.indicator) do
+      c.bg = "#ffffff44"
     end
 
-    if o.urgent then
-      cache.indicator.bg = "#ff000088"
+    local children = wibox.layout.fixed.vertical()
+    children.spacing = config.dpi(1)
+    for i, c in ipairs(o.clients) do
+      if
+        c.active or
+          (capi.client.focus and capi.client.focus.skip_taskbar and
+            capi.client.focus:get_transient_for_matching(
+              function(cl)
+                return not cl.skip_taskbar
+              end
+            ) == c)
+       then
+        cache.w.indicator[i].bg = "#ffffff88"
+        cache.w.indicator[i]:get_children_by_id("size")[1].height = config.dpi(12)
+      end
+
+      if c.urgent then
+        cache.w.indicator[i].bg = "#ff000088"
+        cache.w.indicator[i]:get_children_by_id("size")[1].height = config.dpi(12)
+      end
+
+      local w =
+        wibox.widget {
+        widget = wibox.container.background,
+        bg = "#11111144",
+        {
+          widget = wibox.container.margin,
+          left = config.dpi(15),
+          right = config.dpi(10),
+          top = config.dpi(10),
+          bottom = config.dpi(10),
+          {
+            widget = wibox.container.place,
+            halign = "left",
+            {
+              widget = wibox.widget.textbox,
+              markup = "<span color='#ffffff' font='Inter Medium 11'>" .. c.name .. "</span>"
+            }
+          }
+        }
+      }
+
+      w.buttons = create_buttons(buttons.client, c)
+      children:add(w)
     end
 
-    w:add(cache.primary)
+    cache.popup =
+      awful.popup {
+      widget = children,
+      ontop = true,
+      visible = false,
+      bg = "#11111144",
+      -- placement = awful.placement.next_to(
+      --   cache.popup,
+      --   {
+      --     preferred_positions = "right",
+      --     preferred_anchors = "front",
+      --     margins = {left = config.dpi(5)}
+      --     -- geometry = {x = cache.w.primary.x, y = cache.w.primary.y}
+      --   }
+      -- ),
+      shape = function(cr, width, height)
+        return gears.shape.rounded_rect(cr, width, height, config.dpi(4))
+      end
+    }
+
+    cache.open_popup = function()
+      local s = awful.screen.focused()
+      cache.popup.visible = true
+      cache.popup.screen = s
+    end
+
+    cache.close_popup = function()
+      cache.popup.visible = false
+    end
+
+    w:add(cache.w.primary)
   end
 end
 
 function tasklist.new(screen)
+  local backdrop =
+    wibox {
+    ontop = true,
+    screen = screen,
+    bg = "#ffffff00",
+    type = "utility",
+    x = screen.geometry.x,
+    y = screen.geometry.y,
+    width = screen.geometry.width,
+    height = screen.geometry.height
+  }
+
   return awful.widget.tasklist {
     screen = screen,
     filter = awful.widget.tasklist.filter.allscreen,
     update_function = tasklist.render,
     source = function(s, args)
       local list = {}
-      local cache = {}
       local tags = s.tags
       for k, v in ipairs(tags) do
         for i, c in ipairs(v:clients()) do
-          -- if you want to group same windows you can move table.insert into this if, but i'm not sure how you would access a minimized window
-          if cache ~= nil and c ~= nil and c.class ~= nil and cache[c.class] ~= true then
-            cache[c.class] = true
+          local class = c.class
+
+          local found_group = nil
+          for _, v in ipairs(list) do
+            if v.class == class then
+              found_group = v
+              break
+            end
           end
-          table.insert(list, c)
+
+          if found_group ~= nil then
+            table.insert(found_group.clients, c)
+          else
+            -- create group
+            table.insert(list, {class = class, clients = {c}})
+          end
         end
       end
       return list
@@ -172,43 +264,68 @@ function tasklist.new(screen)
       layout = wibox.layout.fixed.vertical
     },
     buttons = {
-      awful.button(
-        {},
-        1,
-        function(c)
-          if c == client.focus then
-            c.minimized = true
-          else
-            c.minimized = false
-            if not c:isvisible() and c.first_tag then
-              c.first_tag:view_only()
+      client = {
+        awful.button(
+          {},
+          1,
+          function(c)
+            if c == client.focus then
+              c.minimized = true
+            else
+              c.minimized = false
+              if not c:isvisible() and c.first_tag then
+                c.first_tag:view_only()
+              end
+              client.focus = c
+              c:raise()
             end
-            client.focus = c
-            c:raise()
           end
-        end
-      ),
-      awful.button(
-        {awful.util.modkey},
-        2,
-        function(c)
-          c:kill()
-        end
-      ),
-      awful.button(
-        {},
-        4,
-        function()
-          awful.client.focus.byidx(1)
-        end
-      ),
-      awful.button(
-        {},
-        5,
-        function()
-          awful.client.focus.byidx(-1)
-        end
-      )
+        ),
+        awful.button(
+          {awful.util.modkey},
+          2,
+          function(c)
+            c:kill()
+          end
+        ),
+        awful.button(
+          {},
+          4,
+          function()
+            awful.client.focus.byidx(1)
+          end
+        ),
+        awful.button(
+          {},
+          5,
+          function()
+            awful.client.focus.byidx(-1)
+          end
+        )
+      },
+      group = {
+        awful.button(
+          {},
+          1,
+          function(group)
+            backdrop.visible = true
+            backdrop.screen = awful.screen.focused()
+            backdrop:buttons(
+              awful.util.table.join(
+                awful.button(
+                  {},
+                  1,
+                  function()
+                    backdrop.visible = false
+                    group.close_popup()
+                  end
+                )
+              )
+            )
+            group.open_popup()
+          end
+        )
+      }
     }
   }
 end
