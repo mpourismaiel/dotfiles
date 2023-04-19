01b12a7799a7496ce19b263ef5aeb5b9ea302f32
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2023-04-19 16:33:02 +0330

    Extract chrome extension into it's own project

--- extra/chrome-extension/pages/newtab/scripts/handlebars.js
+++
@@ -1,5 +0,0 @@
-const handlebars = require("handlebars/dist/handlebars.runtime");
-globalThis.Handlebars = handlebars;
-require("../build/templates");
-
-module.exports = handlebars;
--- extra/chrome-extension/pages/popup/build/popup.build.js
+++
@@ -1,2 +0,0 @@
-(()=>{chrome.tabs.query({active:!0,currentWindow:!0},function(r){var a=document.getElementById("tabs-list");r.forEach(function(e){var n=document.createElement("li"),t=document.createElement("a");t.textContent=e.title,t.setAttribute("href",e.url),t.setAttribute("target","_blank"),n.appendChild(t),a.appendChild(n)})});})();
-//# sourceMappingURL=popup.build.js.map
