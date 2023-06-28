0ab119875870290aeab1bf95ed9c106f4e98a01d
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2023-06-28 14:48:36 +0330

    Change wallpaper handler

--- rc.lua
+++ rc.lua
@@ -14,6 +14,7 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 local ruled = require("ruled")
+local bling = require("bling")
 local menubar = require("menubar")
 local helpers = require("module.helpers")
 
@@ -46,26 +47,10 @@ require("module.weather")
 require("configuration.bling")
 local widgets = require("configuration.widgets")
 
-screen.connect_signal(
-  "request::wallpaper",
-  function(s)
-    awful.wallpaper {
-      screen = s,
-      widget = {
-        {
-          image = beautiful.wallpaper,
-          upscale = true,
-          downscale = true,
-          widget = wibox.widget.imagebox
-        },
-        valign = "center",
-        halign = "center",
-        tiled = false,
-        widget = wibox.container.tile
-      }
-    }
-  end
-)
+bling.module.wallpaper.setup {
+  wallpaper = {beautiful.wallpaper},
+  position = "fit"
+}
 
 screen.connect_signal(
   "request::desktop_decoration",
