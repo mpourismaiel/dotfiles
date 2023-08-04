866dd10b0d2fe52cd8aa508777314723fb8c7255
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2023-08-04 11:43:27 +0330

    Some stuff

--- rc.lua
+++ rc.lua
@@ -1,6 +1,5 @@
--- awesome_mode: api-level=4:screen=on
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+local collectgarbage = collectgarbage
+collectgarbage("incremental", 110, 1000)
 pcall(require, "luarocks.loader")
 
 require("awful.autofocus")
