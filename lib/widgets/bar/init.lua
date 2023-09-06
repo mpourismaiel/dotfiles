535d7b105eba2f5a9ab0b8aecff3298779d84907
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2023-09-06 21:19:42 +0330

    Fix some other stuff as well

--- lib/widgets/bar/init.lua
+++ lib/widgets/bar/init.lua
@@ -1,6 +1,8 @@
 local capi = {
   awesome = awesome,
-  screen = screen
+  screen = screen,
+  tag = tag,
+  client = client
 }
 local awful = require("awful")
 local wibox = require("wibox")
@@ -12,6 +14,7 @@ local tasklist = require("lib.widgets.tasklist")
 local wbutton = require("lib.widgets.button")
 local datetime = require("lib.widgets.bar.datetime")
 local console = require("lib.helpers.console")
+local debounce = require("lib.helpers.debounce")
 
 local bar = {visible = true, screens = {}}
 
@@ -81,6 +84,59 @@ function bar.toggle()
     end
   end
 end
+
+function bar.handle_fullscreen()
+  local screen = awful.screen.focused()
+  if not screen then
+    return
+  end
+
+  local bar = bar.screens[screen]
+  if not bar then
+    return
+  end
+
+  local tag = screen.selected_tag
+  if not bar then
+    return
+  end
+
+  local focused_client = capi.client.focus
+
+  if focused_client then
+    if focused_client.fullscreen then
+      bar.visible = false
+    else
+      local tag_has_fullscreen = false
+      for _, c in pairs(tag:clients()) do
+        if c.fullscreen and not c.minimized then
+          tag_has_fullscreen = true
+          break
+        end
+      end
+
+      if tag_has_fullscreen then
+        bar.visible = false
+      else
+        bar.visible = true
+      end
+    end
+  else
+    bar.visible = true
+  end
+end
+
+local adjust_visibility = debounce(bar.handle_fullscreen, 0.1)
+
+capi.tag.connect_signal("property::selected", adjust_visibility)
+capi.tag.connect_signal("property::layout", adjust_visibility)
+capi.tag.connect_signal("tagged", adjust_visibility)
+capi.tag.connect_signal("untagged", adjust_visibility)
+capi.tag.connect_signal("property::master_count", adjust_visibility)
+capi.client.connect_signal("property::minimized", adjust_visibility)
+capi.client.connect_signal("property::fullscreen", adjust_visibility)
+capi.client.connect_signal("focus", adjust_visibility)
+capi.client.connect_signal("unfocus", adjust_visibility)
 
 capi.awesome.connect_signal(
   "widget::bar::toggle",
